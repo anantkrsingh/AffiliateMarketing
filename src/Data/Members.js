9a7c10c7440b8a49d784701573b24c86c951fbf6
anantkrsingh <anantkrplay@gmail.com>
2023-05-07 21:47:15 +0530

    team-roles-added

--- src/Data/Members.js
+++ src/Data/Members.js
@@ -1,28 +1,28 @@
 export const Members = [
     {
         name:"Shubham Singh",
-        role:"UI/UX Design",
+        role:"UI/UX Design & Project Management",
         img:"https://lh3.googleusercontent.com/oO569PdegQGP4DLDKc6cZNqTXDWf_NPTVlAr7IMeGFHLgxhojcfF03PUffKqm9QedsNmSM2pYZxaJ3MWfb1Es3pV-jxL5j6xELJ-w51Vpma8pHpIiZicOQxODJtBQwYTNZDVUHu5eQ=w2400",
         about:"Amity University Patna",
         profile:"https://www.linkedin.com/in/shubham-singh-6b10361ba"
     },
     {
         name:"Akash Kumar",
-        role:"",
+        role:"Frontend & Project Management",
         img:"https://firebasestorage.googleapis.com/v0/b/test-16408.appspot.com/o/akash.jpg?alt=media&token=97dbac6b-ba47-46db-9052-f5b2ccc14e39",
         about:"Amity University Patna",
         profile:""
     },
     {
         name:"Shivam Kumar",
-        role:"",
+        role:"Backend",
         img:"https://firebasestorage.googleapis.com/v0/b/test-16408.appspot.com/o/Shivam.jpg?alt=media&token=324cf25c-4791-4fff-b234-a9534b58197c",
         about:"Amity University Patna",
         profile:""
     },
     {
         name:"Aditya Kumar",
-        role:"",
+        role:"CSS Design",
         img:"https://firebasestorage.googleapis.com/v0/b/test-16408.appspot.com/o/Aditya.jpg?alt=media&token=63c85bc1-e09b-4265-822a-873ec883fd5f",
         about:"Amity University Patna",
         profile:""
